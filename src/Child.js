9daf556fb4c3530bccfef7be19737e50798a9d34
hoonkim <hoonk9387@gmail.com>
2022-06-05 19:26:27 -0500

    Completed assignment.

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  console.log(onChangeColor)
+function Child({ onChangeColor, color }) {
+  // console.log(onChangeColor)
+  function handleClick() {
+    const newColor = getRandomColor()
+    /* Callback from Parent. */
+    onChangeColor(newColor)
+  }
+
+
   return <div
-            onClick={onChangeColor} 
+            /* Used onChangeColor as event handler. */
+            // onClick={onChangeColor} 
+            onClick={handleClick}
             className="child" 
-            style={{ backgroundColor: "#FFF" }} 
+            /* color props sent down by Parent */
+            style={{ backgroundColor: color }} 
           />;
 }
 
--- src/Parent.js
+++ src/Parent.js
@@ -5,17 +5,29 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-
-  /* Updates state. */
-  function handleChangeColor() {
+  /* New state variable. Change Child component color.*/
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+  /////////////////////////////////////////// 
+  /* Callback function that Updates state. */
+  ///////////////////////////////////////////
+  function handleChangeColor(newChildColor) {
     const newRandomColor = getRandomColor();
+    //////////////////////////////////////
+    /* Changes state for Parent color. */
+    //////////////////////////////////////
     setColor(newRandomColor);
+    //////////////////////////////////////
+    /* Changes state for Child color. */
+    //////////////////////////////////////
+    setChildrenColor(newChildColor);
   }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child onChangeColor={handleChangeColor} />
-      <Child onChangeColor={handleChangeColor} />
+      {/* Passing handleChangeColor callback as 
+          onChangeColor prop to Child. */}
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
